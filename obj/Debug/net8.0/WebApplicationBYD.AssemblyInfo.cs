2e0941ac4ef04e54fa0e1a17dd8db8239daf7c80
Er2g0n <hnguyenngoc.h@gmail.com>
2024-07-10 15:27:23 +0700

    thay doi view home

--- obj/Debug/net8.0/WebApplicationBYD.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplicationBYD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationBYD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1457f8a28677822e8a0b49a854298e357b9aa456")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebbe9c74986d24871766afb262f8070255de18a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationBYD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationBYD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
